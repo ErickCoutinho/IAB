a40bedca3cdaf21c3556ff3278a6d1c18565a53d
erick_9ge6pj1 <erickcoutinho@alu.ufc.br>
2024-07-30 17:29:46 -0300

    Coparticipação

--- AUTOMAÇÃO_COPARTICIPAÇÃO/leitor_arquivo_unimed.py
+++ AUTOMAÇÃO_COPARTICIPAÇÃO/leitor_arquivo_unimed.py
@@ -5,6 +5,7 @@ class Coparticipacao_Automacao:
         self.caminho_arquivo = caminho_arquivo
         self.dados = []
         self.nomes_valores = {}
+        self.somas_valores = {}  # Dicionário para armazenar as somas dos valores
 
     def ler_arquivo(self):
         """Lê o arquivo e armazena as linhas para processamento posterior."""
@@ -29,7 +30,7 @@ class Coparticipacao_Automacao:
         pattern_bloco = re.compile(r'(\d{13}\s+\d{3}\s+([A-Z\s]+))')
         pattern_valor = re.compile(r'\d{1,3},\d{2}$')  # Regex para capturar valores que estão no final da linha
         nome_atual = None
-        capturando = False  # Flag para controlar a captura de valores
+        capturando = False
 
         for linha in self.linhas:
             nome_match = pattern_bloco.search(linha)
@@ -46,14 +47,22 @@ class Coparticipacao_Automacao:
             if not valor_match and capturando:
                 capturando = False  # Para de capturar valores se uma linha sem valor válido for encontrada
 
-    def exibir_dados(self):
-        """Exibe os nomes e valores associados."""
+    def somar_valores(self):
+        """Converte strings de valores para float e soma os valores para cada nome."""
         for nome, valores in self.nomes_valores.items():
-            print(f"{nome}: {valores}")
+            total = sum(float(valor.replace(',', '.')) for valor in valores)
+            self.somas_valores[nome] = total
+
+    def exibir_somas(self):
+        """Exibe as somas dos valores associados a cada nome."""
+        for nome, soma in self.somas_valores.items():
+            print(f"{nome}: {soma:.2f}")
 
 # Uso do código
 copart = Coparticipacao_Automacao("documents\\PART 07.LST")
 copart.ler_arquivo()
 copart.buscar_nomes()  # Buscar primeiro todos os nomes para garantir a estrutura de dados
 copart.buscar_valores()  # Depois buscar valores associados aos nomes
-copart.exibir_dados()
+copart.somar_valores()   # Somar os valores para cada nome
+copart.exibir_somas()    # Exibir as somas calculadas
+
